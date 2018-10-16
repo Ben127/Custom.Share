7f71b449eb6497956ab5427352f4e77cdf2eec58
Benny <wendys1127@qq.com>
2018-10-16 22:15:23 +0800

    add regexHelper
    add regexHelper and test

---
+++ Custom.Basic.Framework.Test/HelperTest/RegexHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Custom.Basic.Framework.Helper;
+
+namespace Custom.Basic.Framework.Test.HelperTest
+{
+    /// <summary>
+    /// RegexHelperTest
+    /// </summary>
+    public class RegexHelperTest
+    {
+
+        [Fact]
+        public void ReplaceTest()
+        {
+
+            string source = "Hello world, long time no see";
+            string pattern = "world";
+            string result = source.RegexReplace(pattern, "benny");
+
+            Assert.Equal(result, "Hello benny, long time no see");
+        }
+
+    }
+}
---
+++ Custom.Basic.Framework/Helper/RegexHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Custom.Basic.Framework.Helper
+{
+    /// <summary>
+    /// RegexHelper
+    /// </summary>
+    public static class RegexHelper
+    {
+        /// <summary>
+        ///  正则替换
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <param name="input">需替换内容</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <param name="replacement">替换内容</param>
+        /// <param name="regexOptions">正则表达式选项</param>
+        public static string RegexReplace(this string str, string pattern, string replacement, RegexOptions regexOptions)
+        {
+            if (str.IsNullOrWhiteSpace())
+                return str;
+
+            string result = Regex.Replace(str, pattern, replacement, regexOptions);
+            return result;
+        }
+
+        /// <summary>
+        /// 正则替换
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <param name="input">字符串</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <param name="replacement">替换内容</param>
+        /// <returns></returns>
+        public static string RegexReplace(this string str, string pattern, string replacement)
+        {
+            return RegexReplace(str, pattern, replacement, RegexOptions.IgnoreCase);
+        }
+
+
+        /// <summary>
+        ///  正则匹配
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <param name="input">匹配内容</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <param name="regexOptions">正则表达式选项</param>
+        /// <returns></returns>
+        public static MatchCollection Mathes(this string str, string input, string pattern, RegexOptions regexOptions)
+        {
+            return Regex.Matches(input, pattern, regexOptions);
+        }
+
+        /// <summary>
+        ///  正则匹配
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <param name="input">匹配内容</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <returns></returns>
+        public static MatchCollection Mathes(this string str, string input, string pattern)
+        {
+            return Mathes(str, input, pattern, RegexOptions.IgnoreCase);
+        }
+
+
+        /// <summary>
+        ///  正则是否匹配
+        /// </summary>
+        /// <param name="input">匹配内容</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <param name="options">正则表达式选项</param>
+        /// <returns></returns>
+        public static bool IsMatch(string input, string pattern, RegexOptions options)
+        {
+            return Regex.IsMatch(input, pattern, options);
+        }
+
+        /// <summary>
+        ///  正则是否匹配
+        /// </summary>
+        /// <param name="input">匹配内容</param>
+        /// <param name="pattern">正则表达式</param>
+        /// <returns></returns>
+        public static bool IsMatch(string input, string pattern)
+        {
+            return IsMatch(input, pattern, RegexOptions.IgnoreCase);
+        }
+
+
+    }
+}
