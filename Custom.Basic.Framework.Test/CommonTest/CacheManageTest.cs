e09ca2c71c7348ed4845aeb9013c76872fb27219
Benny <wendys1127@qq.com>
2018-10-18 23:40:55 +0800

    add cacheManage .... just
    add cacheManage .... just

---
+++ Custom.Basic.Framework.Test/CommonTest/CacheManageTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Custom.Basic.Framework.Common;
+
+namespace Custom.Basic.Framework.Test.CommonTest
+{
+    /// <summary>
+    /// CacheManageTest
+    /// </summary>
+    public class CacheManageTest
+    {
+
+        public enum Enum_CacheKey
+        {
+            Test,
+            Add
+        }
+
+
+        private void Init()
+        {
+            Dictionary<int, string> cacheList = new Dictionary<int, string>()
+            {
+                {1, "Hello" },
+                {2,"World"},
+                {3,"Benny"},
+                {4,"Nice"}
+            };
+
+            CacheManage.Add<Enum_CacheKey, int, string>(Enum_CacheKey.Test, cacheList);
+
+            Dictionary<string, string> cacheList2 = new Dictionary<string, string>()
+            {
+                {"admin", "123" },
+                {"test","123"},
+                {"sa","sa123"},
+            };
+
+            CacheManage.Add<Enum_CacheKey, string, string>(Enum_CacheKey.Add, cacheList2);
+
+        }
+
+
+        [Fact(DisplayName = "缓存添加")]
+        public void AddTest()
+        {
+            Init();
+            var result = CacheManage.CacheView;
+            Assert.True(CacheManage.Exists(Enum_CacheKey.Test));
+        }
+
+        [Fact(DisplayName = "获取一个字典项字典值")]
+        public void GeTest()
+        {
+            Init();
+
+            string result = CacheManage.Get<Enum_CacheKey, int, string>(Enum_CacheKey.Test, 1);
+            Assert.Equal(result, "Hello");
+
+        }
+
+        [Fact(DisplayName = "移除字典项键Test")]
+        public void RemoveTest()
+        {
+            Init();
+
+            Assert.True(CacheManage.Remove(Enum_CacheKey.Test));
+        }
+
+        [Fact(DisplayName = "移除字典项Add 缓存的某个键")]
+        public void RemoveOneTest()
+        {
+            Init();
+
+            Assert.True(CacheManage.Remove<Enum_CacheKey, string, string>(Enum_CacheKey.Add, "sa"));
+        }
+
+        [Fact(DisplayName = "清空缓存", Skip = "暂不测试")]
+        public void ClearTest()
+        {
+            Init();
+
+            CacheManage.ClearAll();
+
+            Assert.True(CacheManage.CacheView.Count == 0);
+        }
+
+
+    }
+}
---
+++ Custom.Basic.Framework/Common/CacheManage.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom.Basic.Framework.Common
+{
+    /// <summary>
+    /// CacheManage
+    /// </summary>
+    public class CacheManage : ICache
+    {
+
+        private CacheManage()
+        {
+
+        }
+
+        private static CacheManage Instance = new CacheManage();
+
+
+        /// <summary>
+        ///  缓存内容
+        /// </summary>
+        [DebuggerDisplay("当前缓存项： Count = {CacheView.Count}")]
+        public static Dictionary<object, object> CacheView
+        {
+            get
+            {
+                return cacheDictionary;
+            }
+        }
+
+
+        /// <summary>
+        ///  缓存
+        /// </summary>
+        private static Dictionary<object, object> cacheDictionary = new Dictionary<object, object>();
+
+        /// <summary>
+        ///  清空缓存
+        /// </summary>
+        public static void ClearAll()
+        {
+            cacheDictionary.Clear();
+        }
+
+
+        /// <summary>
+        ///  添加缓存
+        /// </summary>
+        /// <typeparam name="CacheKey">缓存值，必须唯一</typeparam>
+        /// <typeparam name="Key">当前缓存列表 Key类型</typeparam>
+        /// <typeparam name="Value">当前缓存列表 Value类型</typeparam>
+        /// <param name="cacheKey">缓存值cacheKey</param>
+        /// <param name="data">当前缓存列表</param>
+        public static void Add<CacheKey, Key, Value>(CacheKey cacheKey, Dictionary<Key, Value> data)
+        {
+            if (!cacheDictionary.ContainsKey(cacheKey) && data != null && data.Count > 0)
+            {
+                cacheDictionary.Add(cacheKey, data);
+                return;
+            }
+
+            throw new Exception("已存在的键值名称" + cacheKey + " 或键值不允许为空");
+        }
+
+        /// <summary>
+        /// 获取缓存值
+        /// </summary>
+        /// <typeparam name="CacheKey">缓存值，必须唯一</typeparam>
+        /// <typeparam name="Key">当前缓存列表 Key类型</typeparam>
+        /// <typeparam name="Value">当前缓存列表 Value类型</typeparam>
+        /// <param name="cacheKey">缓存值cacheKey</param>
+        /// <param name="key">当前缓存列表 Key 键</param>
+        /// <returns></returns>
+        public static Value Get<CacheKey, Key, Value>(CacheKey cachekey, Key key)
+        {
+            if (cacheDictionary.ContainsKey(cachekey))
+            {
+                Dictionary<Key, Value> dictionary = (Dictionary<Key, Value>)cacheDictionary[cachekey];
+                if (dictionary.ContainsKey(key))
+                {
+                    return (Value)dictionary[key];
+                }
+            }
+
+            return default(Value);
+        }
+
+
+        /// <summary>
+        ///  检查是否存在该键值
+        /// </summary>
+        /// <typeparam name="CacheKey">缓存值，必须唯一</typeparam>
+        /// <param name="cacheKey">缓存值cacheKey</param>
+        /// <returns></returns>
+        public static bool Exists<CacheKey>(CacheKey cacheKey)
+        {
+            return cacheDictionary.ContainsKey(cacheKey);
+        }
+
+
+        /// <summary>
+        ///  移除缓存
+        /// </summary>
+        /// <typeparam name="CacheKey">缓存值，必须唯一</typeparam>
+        /// <typeparam name="Key">当前缓存列表 Key类型</typeparam>
+        /// <param name="cacheKey">缓存值cacheKey</param>
+        /// <param name="key">当前缓存列表 Key 键</param>
+        /// <returns></returns>
+        public static bool Remove<CacheKey, Key, Value>(CacheKey cachekey, Key key)
+        {
+            if (cacheDictionary.ContainsKey(cachekey))
+            {
+                var cache = cacheDictionary[cachekey] as Dictionary<Key, Value>;
+                if (cache != null && cache.ContainsKey(key))
+                {
+                    cache.Remove(key);
+                    return true;
+                }
+
+                return false;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        ///  移除缓存项
+        /// </summary>
+        /// <typeparam name="CacheKey">缓存项键名</typeparam>
+        /// <param name="cachekey">缓存项键名</param>
+        /// <returns></returns>
+        public static bool Remove<CacheKey>(CacheKey cachekey)
+        {
+            if (cacheDictionary.ContainsKey(cachekey))
+            {
+                cacheDictionary.Remove(cachekey);
+                return true;
+            }
+
+            return false;
+        }
+
+
+
+    }
+}
