4089984a31b16dc6696dbffbed67a6da8f6454c2
Benny <wendys1127@qq.com>
2018-12-09 21:34:21 +0800

    add package ....

---
+++ Custom.Basic.Framework.Test/PackageTest/ExcelEpplusManagerTest.cs
@@ -0,0 +1,44 @@
+﻿using Custom.Basic.Framework.Package.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Custom.Basic.Framework.Test.PackageTest
+{
+    /// <summary>
+    /// ExcelEpplusManagerTest
+    /// </summary>
+    public class ExcelEpplusManagerTest
+    {
+
+        public class Books
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        [Fact]
+        public void ExportTest()
+        {
+            List<Books> result = new List<Books>()
+            {
+                new Books(){ Id=100, Name="西游记"},
+                new Books(){ Id=100, Name="三国演义"},
+                new Books(){ Id=100, Name="红楼梦"},
+                new Books(){ Id=100, Name="大话西游"},
+                new Books(){ Id=100, Name="日夜"},
+                new Books(){ Id=100, Name="叶子"}
+            };
+            ExcelEpplusManager.Export(result, "test.xlsx");
+
+            FileInfo fi = new FileInfo("test.xlsx");
+
+            Assert.True(File.Exists(fi.FullName));
+        }
+
+    }
+}
---
+++ Custom.Basic.Framework/Package/Excel/ExcelEpplusManager.cs
@@ -0,0 +1,60 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Table;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom.Basic.Framework.Package.Excel
+{
+    /// <summary>
+    /// Epplus Excel 导出
+    /// </summary>
+    public class ExcelEpplusManager
+    {
+        /// <summary>
+        /// 泛型列表导出Excel
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="datas">数据</param>
+        /// <param name="excelName">Excel文件名</param>
+        /// <param name="password">密码</param>
+        public static void Export<T>(List<T> datas, string excelName, string password = "")
+        {
+            using (var p = new ExcelPackage())
+            {
+                var ws = p.Workbook.Worksheets.Add("sheet");
+                ws.Cells["A1"].LoadFromCollection(datas, true, TableStyles.Light12);
+
+                p.SaveAs(new FileInfo(excelName), password);
+            }
+
+        }
+
+        /// <summary>
+        ///  DataTable 导出Excel
+        /// </summary>
+        /// <param name="datas">数据</param>
+        /// <param name="excelName">Excel文件名</param>
+        /// <param name="password">密码</param>
+        public static void Export(DataTable datas, string excelName, string password = "")
+        {
+            using (var p = new ExcelPackage())
+            {
+                var ws = p.Workbook.Worksheets.Add("sheet");
+                ws.Cells["A1"].LoadFromDataTable(datas, true);
+
+                p.SaveAs(new FileInfo(excelName), password);
+            }
+
+        }
+
+
+
+
+
+    }
+}
