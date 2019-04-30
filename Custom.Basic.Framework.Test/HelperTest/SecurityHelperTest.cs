2f8b3e961278ba35ff2c46d05070b6710e70e449
Benny <wendys1127@qq.com>
2019-04-30 22:34:58 +0800

    a little change...

---
+++ Custom.Basic.Framework.Test/HelperTest/SecurityHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using Custom.Basic.Framework.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Custom.Basic.Framework.Test.HelperTest
+{
+    /// <summary>
+    /// SecurityHelperTest
+    /// </summary>
+    public class SecurityHelperTest
+    {
+        [Fact]
+        public void RSAEncryptionTest()
+        {
+            string source = "Hello world";
+            string result = SecurityHelper.RSAEncryption(source);
+
+            string source2 = SecurityHelper.RSADecrypt(result);
+
+            Assert.Equal(source, source2);
+
+        }
+
+
+
+    }
+}
---
+++ Custom.Basic.Framework/Helper/SecurityHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom.Basic.Framework.Helper
+{
+    /// <summary>
+    /// 加解密
+    /// </summary>
+    public class SecurityHelper
+    {
+        private const string Key = "KEY_12321";
+
+        public static string RSAEncryption(string express)
+        {
+            CspParameters param = new CspParameters();
+            param.KeyContainerName = Key;//密匙容器的名称，保持加密解密一致才能解密成功
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(param))
+            {
+                byte[] plaindata = Encoding.Default.GetBytes(express);//将要加密的字符串转换为字节数组
+                byte[] encryptdata = rsa.Encrypt(plaindata, false);//将加密后的字节数据转换为新的加密字节数组
+                return Convert.ToBase64String(encryptdata);//将加密后的字节数组转换为字符串
+            }
+        }
+
+        //解密
+        public static string RSADecrypt(string ciphertext)
+        {
+            CspParameters param = new CspParameters();
+            param.KeyContainerName = Key;
+            using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(param))
+            {
+                byte[] encryptdata = Convert.FromBase64String(ciphertext);
+                byte[] decryptdata = rsa.Decrypt(encryptdata, false);
+                return Encoding.Default.GetString(decryptdata);
+            }
+        }
+
+    }
+}
