1559bb151c227b778aaadc3852a127b4ff28c4f6
Benny <wendys1127@qq.com>
2019-05-31 23:36:21 +0800

    a little change...

---
+++ Custom.Winform/FrmDatagridView.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Custom.Winform
+{
+    partial class FrmDatagridView
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnInit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(775, 358);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 395);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 35);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "添加一行";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(115, 395);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 35);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "删除一行";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(219, 395);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(88, 35);
+            this.btnUpdate.TabIndex = 1;
+            this.btnUpdate.Text = "更新一行";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(323, 395);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(88, 35);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "清空全部";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(426, 395);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(137, 35);
+            this.btnInit.TabIndex = 1;
+            this.btnInit.Text = "随机初始化十条";
+            this.btnInit.UseVisualStyleBackColor = true;
+            this.btnInit.Click += new System.EventHandler(this.btnInit_Click);
+            // 
+            // FrmDatagridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 453);
+            this.Controls.Add(this.btnInit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmDatagridView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "datagridview-自动绑定刷新数据";
+            this.Load += new System.EventHandler(this.FrmDatagridView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnInit;
+    }
+}
+
---
+++ Custom.Winform/FrmDatagridView.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Custom.Winform
+{
+    public partial class FrmDatagridView : Form
+    {
+        public FrmDatagridView()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDatagridView_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private BindingList<User> List = new BindingList<User>();
+
+
+        public class User : INotifyPropertyChanged
+        {
+            public User()
+            {
+                userId = Guid.NewGuid().ToString("N");
+            }
+
+            private string userId;
+
+            public string UserId
+            {
+                get { return userId; }
+                set
+                {
+                    if (userId != value)
+                    {
+                        userId = value;
+                        PropertyChanged(this, new PropertyChangedEventArgs("UserId"));
+                    }
+                }
+            }
+            private DateTime date;
+
+            public DateTime Date
+            {
+                get { return date; }
+                set
+                {
+                    if (date != value)
+                    {
+                        date = value;
+                        PropertyChanged(this, new PropertyChangedEventArgs("Date"));
+                    }
+                }
+            }
+
+            private string message;
+
+            public string Message
+            {
+                get { return message; }
+                set
+                {
+                    if (message != value)
+                    {
+                        message = value;
+                        PropertyChanged(this, new PropertyChangedEventArgs("Message"));
+                    }
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged = (sender, args) => { };
+        }
+
+        private void btnInit_Click(object sender, EventArgs e)
+        {
+            List.Clear();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var u = new User() { Date = DateTime.Now };
+                List.Add(u);
+            }
+
+            dataGridView1.DataSource = List;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var u = new User() { Date = DateTime.Now };
+            List.Add(u);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            List.Remove(List.LastOrDefault());
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var u = List.OrderBy(p => Guid.NewGuid()).FirstOrDefault();
+            if (u != null)
+                u.Message = "123";
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            List.Clear();
+        }
+
+
+
+    }
+}
