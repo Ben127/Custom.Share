7a4c7894d0e8f395c575abaa8502f05854bcdb1f
Benny <wendys1127@qq.com>
2019-07-30 23:16:06 +0800

    ....

---
+++ Custom.Basic.Framework/Helper/CSVHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom.Basic.Framework.Helper
+{
+    /// <summary>
+    /// CSVHelper
+    /// </summary>
+    public class CSVHelper
+    {
+        public static List<T> Read<T>(string fullFileName, bool hasHeader = true)
+        {
+            using (var reader = new StreamReader(fullFileName))
+            {
+                using (var csv = new CsvHelper.CsvReader(reader))
+                {
+                    csv.Configuration.HasHeaderRecord = hasHeader;
+                    var result = csv.GetRecords<T>().ToList();
+
+                    return result;
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ Custom.Basic.Framework/Helper/GzipHelper.cs
@@ -0,0 +1,38 @@
+﻿using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.GZip;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custom.Basic.Framework.Helper
+{
+    /// <summary>
+    /// GzipHelper
+    /// </summary>
+    public class GzipHelper
+    {
+        public static void ExtractGZipSample(string gzipFileName, string targetDir)
+        {
+            // Use a 4K buffer. Any larger is a waste.    
+            byte[] dataBuffer = new byte[4096];
+
+            using (System.IO.Stream fs = new FileStream(gzipFileName, FileMode.Open, FileAccess.Read))
+            {
+                using (GZipInputStream gzipStream = new GZipInputStream(fs))
+                {
+                    // Change this to your needs
+                    string fnOut = Path.Combine(targetDir, Path.GetFileNameWithoutExtension(gzipFileName));
+
+                    using (FileStream fsOut = File.Create(fnOut))
+                    {
+                        StreamUtils.Copy(gzipStream, fsOut, dataBuffer);
+                    }
+                }
+            }
+        }
+
+    }
+}
